17f656b7a0b049757d7f41d71cc51b051993a82c
theunreal13 <91556509+theunreal13@users.noreply.github.com>
2022-03-05 20:47:59 -0600

    Add files via upload

---
+++ pages/contact.js
@@ -0,0 +1,318 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Contact = (props) => {
+  return (
+    <>
+      <div className="container">
+        <Head>
+          <title>Contact - The Unreal 13</title>
+          <meta property="og:title" content="Contact - The Unreal 13" />
+        </Head>
+        <header data-role="Header" className="header">
+          <img
+            alt="logo"
+            src="https://presentation-website-assets.teleporthq.io/logos/logo.png"
+            className="image"
+          />
+          <div className="nav"></div>
+          <div className="btn-group">
+            <button className="login button">Login</button>
+            <button className="button">Register</button>
+          </div>
+          <div data-type="BurgerMenu" className="burger-menu">
+            <svg viewBox="0 0 1024 1024" className="icon">
+              <path d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
+            </svg>
+          </div>
+          <div data-type="MobileMenu" className="mobile-menu">
+            <div className="nav1">
+              <div className="container1">
+                <img
+                  alt="image"
+                  src="https://presentation-website-assets.teleporthq.io/logos/logo.png"
+                  className="image1"
+                />
+                <div data-type="CloseMobileMenu" className="menu-close">
+                  <svg viewBox="0 0 1024 1024" className="icon02">
+                    <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div>
+              <svg viewBox="0 0 950.8571428571428 1024" className="icon04">
+                <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+              </svg>
+              <svg viewBox="0 0 877.7142857142857 1024" className="icon06">
+                <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+              </svg>
+              <svg viewBox="0 0 602.2582857142856 1024" className="icon08">
+                <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
+              </svg>
+            </div>
+          </div>
+        </header>
+        <div className="hero">
+          <h1 className="text">Having Questions?</h1>
+          <span className="text1">
+            <span>
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+                volutpat turpis.
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span>
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+            </span>
+            <br></br>
+            <span>
+              <span>
+                Mauris luctus rutrum mi ut rhoncus. Integer in dignissim tortor.
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span>
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+            </span>
+          </span>
+          <div className="btn-group1">
+            <button className="button">Get Started</button>
+            <button className="button1 button">Learn More</button>
+          </div>
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+          .header {
+            width: 100%;
+            display: flex;
+            max-width: var(--dl-size-size-maxwidth);
+            align-items: center;
+            padding-top: var(--dl-space-space-twounits);
+            padding-left: var(--dl-space-space-threeunits);
+            padding-right: var(--dl-space-space-threeunits);
+            padding-bottom: var(--dl-space-space-twounits);
+            justify-content: space-between;
+          }
+          .image {
+            height: 2rem;
+          }
+          .nav {
+            display: flex;
+          }
+          .btn-group {
+            display: flex;
+            align-items: center;
+            flex-direction: row;
+            justify-content: space-between;
+          }
+          .login {
+            border-width: 0px;
+            margin-right: var(--dl-space-space-twounits);
+          }
+          .burger-menu {
+            display: none;
+          }
+          .icon {
+            width: var(--dl-size-size-xsmall);
+            height: var(--dl-size-size-xsmall);
+          }
+          .mobile-menu {
+            top: 0px;
+            left: 0px;
+            width: 100%;
+            height: 100vh;
+            display: none;
+            padding: 32px;
+            z-index: 100;
+            position: absolute;
+            flex-direction: column;
+            justify-content: space-between;
+            background-color: #fff;
+          }
+          .nav1 {
+            display: flex;
+            align-items: flex-start;
+            flex-direction: column;
+          }
+          .container1 {
+            width: 100%;
+            display: flex;
+            align-items: center;
+            margin-bottom: var(--dl-space-space-threeunits);
+            justify-content: space-between;
+          }
+          .image1 {
+            height: 2rem;
+          }
+          .menu-close {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+          .icon02 {
+            width: var(--dl-size-size-xsmall);
+            height: var(--dl-size-size-xsmall);
+          }
+          .icon04 {
+            width: var(--dl-size-size-xsmall);
+            height: var(--dl-size-size-xsmall);
+            margin-right: var(--dl-space-space-twounits);
+          }
+          .icon06 {
+            width: var(--dl-size-size-xsmall);
+            height: var(--dl-size-size-xsmall);
+            margin-right: var(--dl-space-space-twounits);
+          }
+          .icon08 {
+            width: var(--dl-size-size-xsmall);
+            height: var(--dl-size-size-xsmall);
+          }
+          .hero {
+            width: 100%;
+            display: flex;
+            padding: var(--dl-space-space-threeunits);
+            position: relative;
+            min-height: 80vh;
+            align-items: center;
+            flex-direction: column;
+            background-size: cover;
+            justify-content: center;
+            background-image: linear-gradient(
+                113.8deg,
+                rgba(255, 76, 92, 0.8) 7.5%,
+                rgba(255, 177, 70, 0.8) 94.47%
+              ),
+              url('https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDIzfHx3b3JrJTIwbGFwdG9wfGVufDB8fHx8MTYyNjE3NjkzMA&ixlib=rb-1.2.1&h=1200');
+          }
+          .text {
+            color: var(--dl-color-gray-white);
+            font-size: 3rem;
+            font-family: Krona One;
+          }
+          .text1 {
+            color: var(--dl-color-gray-white);
+            margin-top: var(--dl-space-space-twounits);
+            margin-bottom: var(--dl-space-space-twounits);
+          }
+          .btn-group1 {
+            display: flex;
+            align-items: center;
+            flex-direction: row;
+          }
+          .button {
+            font-size: 0.75rem;
+            transition: 0.3s;
+            padding-top: var(--dl-space-space-unit);
+            border-width: 0px;
+            padding-left: var(--dl-space-space-twounits);
+            padding-right: var(--dl-space-space-twounits);
+            padding-bottom: var(--dl-space-space-unit);
+            text-transform: uppercase;
+          }
+          .button:hover {
+            transform: scale(1.02);
+          }
+          .button1 {
+            color: var(--dl-color-gray-white);
+            font-size: 0.75rem;
+            transition: 0.3s;
+            margin-left: var(--dl-space-space-unit);
+            padding-top: var(--dl-space-space-unit);
+            border-color: var(--dl-color-gray-white);
+            padding-left: var(--dl-space-space-twounits);
+            padding-right: var(--dl-space-space-twounits);
+            padding-bottom: var(--dl-space-space-unit);
+            text-transform: uppercase;
+            background-color: transparent;
+          }
+          .button1:hover {
+            transform: scale(1.02);
+          }
+          @media (max-width: 991px) {
+            .hero {
+              flex-direction: column;
+            }
+            .text {
+              text-align: center;
+            }
+            .text1 {
+              text-align: center;
+            }
+          }
+          @media (max-width: 767px) {
+            .header {
+              padding-left: var(--dl-space-space-twounits);
+              padding-right: var(--dl-space-space-twounits);
+            }
+            .nav {
+              display: none;
+            }
+            .btn-group {
+              display: none;
+            }
+            .burger-menu {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+            }
+            .hero {
+              padding-left: var(--dl-space-space-twounits);
+              padding-right: var(--dl-space-space-twounits);
+            }
+          }
+          @media (max-width: 479px) {
+            .header {
+              padding: var(--dl-space-space-unit);
+            }
+            .mobile-menu {
+              padding: 16px;
+            }
+            .hero {
+              width: 100%;
+              padding: var(--dl-space-space-twounits);
+              padding-top: var(--dl-space-space-twounits);
+              padding-left: var(--dl-space-space-unit);
+              padding-right: var(--dl-space-space-unit);
+            }
+            .btn-group1 {
+              flex-direction: column;
+            }
+            .button1 {
+              margin-top: var(--dl-space-space-unit);
+              margin-left: 0px;
+            }
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Contact
